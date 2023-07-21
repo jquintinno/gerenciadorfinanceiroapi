f7adfe141c18e8c557eee996f83fa7a584335ff3
Quintinno Digital <jquintinno@gmail.com>
2023-07-21 10:35:38 -0300

    Feature: Implementar Regras Negociais da Funcionalidade Gerenciador de Transações Financeiras (GERTRAFIN)

--- src/main/java/br/com/quintinno/gerenciadorfinanceiroapi/service/ParcelamentoService.java
+++ src/main/java/br/com/quintinno/gerenciadorfinanceiroapi/service/ParcelamentoService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.quintinno.gerenciadorfinanceiroapi.domain.ParcelamentoDomain;
 import br.com.quintinno.gerenciadorfinanceiroapi.domain.TransacaoFinanceiraDomain;
+import br.com.quintinno.gerenciadorfinanceiroapi.enumeration.TipoTransacaoFinanceiraEnumeration;
 import br.com.quintinno.gerenciadorfinanceiroapi.repository.ParcelamentoRepository;
 import br.com.quintinno.gerenciadorfinanceiroapi.utility.DateUtility;
 import jakarta.transaction.Transactional;
@@ -28,7 +29,8 @@ public class ParcelamentoService {
 			parcelamentoDomain.setNumero(numeroParcela);
 			parcelamentoDomain.setValor(transacaoFinanceiraDomain.getValorUnitario());
 			transacaoFinanceiraDomain.setDataHoraCadastro(LocalDateTime.now());
-			parcelamentoDomain.setDataVencimento(this.gerarDataVencimento(transacaoFinanceiraDomain.getDiaVencimento(), transacaoFinanceiraDomain.getDataHoraCadastro(), numeroParcela));
+			Boolean bolMesAtual = transacaoFinanceiraDomain.getTipoTransacaoFinanceiraDomain().getCodigo() == TipoTransacaoFinanceiraEnumeration.DESPESA_VARIAVEL.getCodigo() && transacaoFinanceiraDomain.getQuantidadeParcela() == 1 ? true : false;
+			parcelamentoDomain.setDataVencimento(this.gerarDataVencimento(transacaoFinanceiraDomain.getDiaVencimento(), transacaoFinanceiraDomain.getDataHoraCadastro(), numeroParcela, bolMesAtual));	
 			this.parcelamentoRepository.save(parcelamentoDomain);
 	}
 	
@@ -36,8 +38,8 @@ public class ParcelamentoService {
 		return PREFIXO.concat(DateUtility.recuperarDataAtual(DateUtility.FORMATO_B));
 	}
 	
-	private LocalDate gerarDataVencimento(Integer diaVencimento, LocalDateTime dataHoraCadastro, int index) {
-		return dataHoraCadastro.toLocalDate().plusMonths(index);
+	private LocalDate gerarDataVencimento(Integer diaVencimento, LocalDateTime dataHoraCadastro, int index, Boolean bolMesAtual) {
+		return bolMesAtual ? dataHoraCadastro.toLocalDate() : dataHoraCadastro.toLocalDate().plusMonths(index);
 	}
 
 }
